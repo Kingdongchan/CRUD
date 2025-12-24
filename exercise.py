30c9f2b3f3d5837a8b6b62b0b61624b7ddcdf943
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-24 15:06:28 +0900

    이유 쓰기 완료

---
+++ exercise.py
@@ -0,0 +1,10 @@
+# 운동을 해야하는 이유를 담을 곳
+
+exercise_reason = []
+
+def do_reason(reason, wirter):
+    add_reaon = f"{reason} - {wirter}"
+    exercise_reason.append(add_reaon)
+    print("등록완료!")
+
+do_reason("탈모예방효과","김동찬")
