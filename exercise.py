a14750c7ea6ec51ddc5670b98c2fcf332d40e47d
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-24 15:25:09 +0900

    추가목록 만듬

--- exercise.py
+++ exercise.py
@@ -3,11 +3,20 @@
 exercise_reason = []
 
 # 사용자가 원하는 행동
-user_input = input("(목록)/(추가)/(삭제)/(수정)")
+user_input = input("(목록)/(추가)/(삭제)/(수정):")
 
-#목록을 원한다면
-
-
+#목록 선택
 if user_input == "목록":
-    print("현재 들어있는 조언 목록들입니다.")
+    print("--현재 들어있는 조언 목록들입니다.--")
     print(exercise_reason) 
+
+# 추가 선택
+elif user_input == "추가":
+    user_input_reason= input("내용: ")
+    user_input_name = input("작성자: ")
+    
+    def add_reason(user_input_reason, user_input_name):
+        list_reason = {"번호":len(exercise_reason)+ 1, "조언": user_input_reason, "작성자":user_input_name}
+        
+        exercise_reason.append(list_reason)
+        print("---등록 완료---")
