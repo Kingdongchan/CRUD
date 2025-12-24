72afc66d1f3acd7087eac1c50f677f3353a46bb9
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-24 15:45:24 +0900

    수정 만들기

--- exercise.py
+++ exercise.py
@@ -7,7 +7,7 @@ user_input = input("(목록)/(추가)/(삭제)/(수정):")
 
 #목록 선택
 if user_input == "목록":
-    print("--현재 들어있는 조언 게시물 목록들입니다.--")
+    print("--현재 들어있는 조언 게시물 목록들 입니다.--")
     print(exercise_reason) 
 
 # 추가 선택
@@ -24,8 +24,12 @@ elif user_input == "추가":
 # 삭제 선택
 elif user_input == "삭제":
         reason_number = len(exercise_reason)+1
-        print(f"-{reason_number}.", exercise_reason)
-        user_choose_number = int(input("삭제할 게시물의 숫자: "))
+        #목록 불러오기
+        for j in range(len(exercise_reason)):
+            print(f"-{reason_number}.", exercise_reason)
+        
+        user_choose_number = int(input("삭제할 게시물의 번호: "))
+        
         # 목록에 있는 것들 나열하기
         for i in range(len(exercise_reason)):
              exercise_number = exercise_reason[i]
@@ -35,5 +39,19 @@ elif user_input == "삭제":
 
                   print(f"{user_choose_number}번 삭제 완료")
 
-print(exercise_number)
-    
+# 수정 선택
+elif user_input == "수정":
+    reason_number = len(exercise_reason)+1
+        #목록 불러오기
+    for j in range(len(exercise_reason)):
+        print(f"-{reason_number}.", exercise_reason)
+        
+    user_choose_number = int(input("수정할 게시물의 번호: "))
+    #운동 조언 목록 가져오기
+    for i in range(len(exercise_reason)):
+         exercise_number = exercise_reason[i]
+        # 유저가 고른 번호와 맞는지 확인
+         if user_choose_number == exercise_number:
+              #수정
+              user_repost = input("수정 할 내용: ")
+              exercise_reason[i]["조언"] = user_repost
